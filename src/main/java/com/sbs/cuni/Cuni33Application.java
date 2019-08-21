ac308a0988e25c994d5cf40d7f2581673cc134b4
jw02048 <jw02048@naver.com>
2019-08-21 09:54:16 +0900

    초기 상태

---
+++ src/main/java/com/sbs/cuni/Cuni33Application.java
@@ -0,0 +1,13 @@
+package com.sbs.cuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cuni33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cuni33Application.class, args);
+	}
+
+}
